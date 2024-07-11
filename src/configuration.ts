5db51f10a3ccf4e818fd351c2a70513b8949641d
Ramin <raminramazanpour@gmail.com>
2024-07-11 13:57:25 +0330

    test dev

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,7 +3,6 @@ export const isDevelopment = process.env.NEXT_PUBLIC_ENV === 'development';
 const BASE_ROUTE = isDevelopment
 	? 'https://impact-graph.serve.giveth.io'
 	: 'https://mainnet.serve.giveth.io';
-
 const config = {
 	BACKEND_LINK: `${BASE_ROUTE}/graphql`,
 };
