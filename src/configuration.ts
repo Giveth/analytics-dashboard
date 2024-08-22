04cda235f10972afb639ac05cb66b7ea0da8affc
Reshzera <reoshiro13@gmail.com>
2024-08-22 18:03:14 -0300

    feat: change project label text

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,8 +1,8 @@
 export const isDevelopment = process.env.NEXT_PUBLIC_ENV === 'development';
 
 const BASE_ROUTE = isDevelopment
-	? 'https://impact-graph.serve.giveth.io'
-	: 'https://mainnet.serve.giveth.io';
+	? 'http://localhost:4000'
+	: 'http://localhost:4000';
 
 const SWIE_AUTH_ROUTE = isDevelopment
 	? 'https://auth.serve.giveth.io'
