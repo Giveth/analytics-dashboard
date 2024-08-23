c1fc6c7a616179a0df8e239a6730cda977dc7269
Reshzera <reoshiro13@gmail.com>
2024-08-23 11:03:17 -0300

    fix: change url base route

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,8 +1,8 @@
 export const isDevelopment = process.env.NEXT_PUBLIC_ENV === 'development';
 
 const BASE_ROUTE = isDevelopment
-	? 'http://localhost:4000'
-	: 'http://localhost:4000';
+	? 'https://impact-graph.serve.giveth.io'
+	: 'https://mainnet.serve.giveth.io';
 
 const SWIE_AUTH_ROUTE = isDevelopment
 	? 'https://auth.serve.giveth.io'
