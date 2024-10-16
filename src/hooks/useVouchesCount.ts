35a31b95afd98c8d688c5a036d884e3b82b16eb0
Cherik <Pourcheriki@gmail.com>
2024-10-16 13:06:18 +0330

    update useVouchesCount

--- src/hooks/useVouchesCount.ts
+++ src/hooks/useVouchesCount.ts
@@ -2,21 +2,21 @@ import { useEffect, useState } from 'react';
 import { formatDateToISO, showToastError } from '../lib/helpers';
 import { deVouchGQLRequest } from '../lib/requests';
 import { FETCH_VOUCHES_COUNT_BY_DATE } from '../gql/gqlVerification';
-import { IFetchVouchesCountRes } from '../types/gql';
+import { IFetchVouchesCountRes, IVouchCountInfo } from '../types/gql';
 
 export const useVouchesCount = (
 	fromDate: Date,
 	toDate: Date,
 	organizationId: string,
 ) => {
-	const [vouchCountInfo, setVouchCountInfo] = useState({});
+	const [vouchCountInfo, setVouchCountInfo] = useState<IVouchCountInfo>();
 	const [loading, setLoading] = useState<boolean>(true);
 	useEffect(() => {
 		!loading && setLoading(true);
 		const variables = {
 			fromDate: formatDateToISO(fromDate),
 			toDate: formatDateToISO(toDate),
-			organizationId,
+			organisationId: organizationId,
 		};
 		deVouchGQLRequest(FETCH_VOUCHES_COUNT_BY_DATE, variables)
 			.then((res: IFetchVouchesCountRes) => {
@@ -24,7 +24,7 @@ export const useVouchesCount = (
 			})
 			.catch(showToastError)
 			.finally(() => setLoading(false));
-	}, []);
+	}, [fromDate, organizationId, toDate]);
 
 	return { vouchCountInfo, loading };
 };
