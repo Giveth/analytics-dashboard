f76a269247f2a59380467614bb920d7a92185a48
Cherik <Pourcheriki@gmail.com>
2024-10-16 13:56:53 +0330

    add totalWithComments to the query

--- src/gql/gqlVerification.ts
+++ src/gql/gqlVerification.ts
@@ -1,6 +1,7 @@
 export const FETCH_VOUCHES_COUNT_BY_DATE = `query MyQuery($fromDate: String = "", $organisationId: String = "", $toDate: String = "") {
 	getOrganisationVouchCountByDate(fromDate: $fromDate, organisationId: $organisationId, toDate: $toDate) {
 	  total
+	  totalWithComments
 	  totalPerMonth {
 		countWithComments
 		countWithoutComments
