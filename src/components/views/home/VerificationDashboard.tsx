f9e57f576370019e1e2fdbfd97d2c9568455173f
Cherik <Pourcheriki@gmail.com>
2024-10-16 14:12:22 +0330

    add verification chart

--- src/components/views/home/VerificationDashboard.tsx
+++ src/components/views/home/VerificationDashboard.tsx
@@ -9,7 +9,11 @@ import {
 import styled from 'styled-components';
 import React, { useState } from 'react';
 import { Col, Row } from '../../styled-components/grid';
-import { firstOfNextMonth, firstOfThisMonth } from '../../../lib/helpers';
+import {
+	firstOfNextMonth,
+	firstOfThisMonth,
+	formatAmount,
+} from '../../../lib/helpers';
 import { IconWithTooltip } from '../../IconWithTooltip';
 import { Flex, FlexCenter } from '../../styled-components/flex';
 import DatePicker from '../../DatePicker';
@@ -17,11 +21,12 @@ import CheckBox from '../../CheckBox';
 import { useVouchesCount } from '../../../hooks/useVouchesCount';
 import { GIVETH_VERIFIERS_ORG_ID } from '../../../lib/constants';
 import Spinner from '../../Spinner';
+import VerificationChart from './charts/VerificationChart';
 
 const VerificationDashboard = () => {
 	const [fromDate, setFromDate] = useState(firstOfThisMonth());
 	const [toDate, setToDate] = useState(firstOfNextMonth());
-	const [onlyWithComment, setOnlyWithComment] = useState(false);
+	const [showComment, setShowComment] = useState(true);
 
 	const { vouchCountInfo, loading } = useVouchesCount(
 		fromDate,
@@ -60,9 +65,9 @@ const VerificationDashboard = () => {
 					<FilterRows gap='10px' wrap={1}>
 						<H6>Filters:</H6>
 						<CheckBox
-							checked={onlyWithComment}
-							onChange={setOnlyWithComment}
-							label='Show only vouches with comments'
+							checked={showComment}
+							onChange={setShowComment}
+							label='Show vouches with comments info'
 							size={16}
 						/>
 					</FilterRows>
@@ -75,17 +80,26 @@ const VerificationDashboard = () => {
 						{loading ? (
 							<Spinner />
 						) : (
-							<H2>{vouchCountInfo?.total}</H2>
+							<H2>
+								{formatAmount(
+									showComment
+										? vouchCountInfo?.totalWithComments || 0
+										: vouchCountInfo?.total || 0,
+								)}
+							</H2>
 						)}
 					</FlexCenter>
 				</Col>
 			</RowStyled>
 			<Col md={2}></Col>
-			{/* {loading ? (
+			{loading ? (
 				<Spinner />
 			) : (
-				<ProjectsChart totalPerMonthAndYear={totalPerMonthAndYear!} />
-			)} */}
+				<VerificationChart
+					vouchCountsPerMonth={vouchCountInfo?.totalPerMonth!}
+					showWithComments={showComment}
+				/>
+			)}
 		</>
 	);
 };
