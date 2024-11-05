94037ed616a368f237adb44ab5122657ad594fc1
Cherik <Pourcheriki@gmail.com>
2024-11-05 12:23:05 +0330

    update types

--- src/types/gql.ts
+++ src/types/gql.ts
@@ -120,12 +120,14 @@ export interface IFetchVouchesCountRes {
 
 export interface IVouchCountByUser {
 	totalCount: number;
+	countWithComments: number;
 	attestorId: string;
 }
 
 export interface IAttestorVouchesCountToSource {
 	vouchCountByUser: IVouchCountByUser[];
 	totalVouches: number;
+	totalWithComments: number;
 }
 
 export interface IAttestorVouchesCountToSourceRes {
