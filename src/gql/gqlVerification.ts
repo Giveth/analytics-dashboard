f9388ae747a1f87983168f0813ed0d4960379fee
Cherik <Pourcheriki@gmail.com>
2024-11-05 11:37:00 +0330

    add FETCH_USER_VOUCHES_COUNT_TO_SOURCE_BY_DATE

--- src/gql/gqlVerification.ts
+++ src/gql/gqlVerification.ts
@@ -10,3 +10,14 @@ export const FETCH_VOUCHES_COUNT_BY_DATE = `query MyQuery($fromDate: String = ""
 	  }
 	}
   }`;
+
+export const FETCH_USER_VOUCHES_COUNT_TO_SOURCE_BY_DATE = `query MyQuery($fromDate: String = "", $organisationId: String = "", $source: String = "", $toDate: String = "") {
+  getOrganisationUserVouchCountBySource(fromDate: $fromDate, organisationId: $organisationId, source: $source, toDate: $toDate) {
+    
+    vouchCountByUser {
+      totalCount
+      attestorId
+    }
+    totalVouches
+  }
+}`;
