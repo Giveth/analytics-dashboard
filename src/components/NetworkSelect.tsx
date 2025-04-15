1c7ac9b37c13f4e31cef68c6bc28cf4352ae8adf
kkatusic <kkatusic@gmail.com>
2025-04-15 11:21:07 +0200

    Revert "New release"

--- src/components/NetworkSelect.tsx
+++ src/components/NetworkSelect.tsx
@@ -39,10 +39,6 @@ function getCorrectNetworkIdBasedOnEnv(networkId: number) {
 			return isDevelopment
 				? NETWORK_IDS.MORDOR_ETC_TESTNET
 				: NETWORK_IDS.ETC;
-		case NETWORK_IDS.STELLAR_MAINNET:
-			return isDevelopment
-				? NETWORK_IDS.STELLAR_TESTNET
-				: NETWORK_IDS.STELLAR_MAINNET;
 		default:
 			return networkId;
 	}
@@ -114,13 +110,6 @@ const NetworkSelect: React.FC<NetworkSelectProps> = ({
 				</option>
 				<option value={getCorrectNetworkIdBasedOnEnv(NETWORK_IDS.CELO)}>
 					Celo
-				</option>
-				<option
-					value={getCorrectNetworkIdBasedOnEnv(
-						NETWORK_IDS.STELLAR_MAINNET,
-					)}
-				>
-					Stellar
 				</option>
 			</SelectStyled>
 		</>
