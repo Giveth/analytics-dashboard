c3c71ea975e46393dde28717733b3f1a8f062191
Ramin <raminramazanpour@gmail.com>
2026-09-24 01:07:12 +0330

    Refactor TabContent component to utilize TABS enum and pass backend version to DonationsTab for improved structure and maintainability

--- src/components/views/home/TabContent.tsx
+++ src/components/views/home/TabContent.tsx
@@ -11,56 +11,51 @@ import RecurringDonationsCount from './RecurringDonationsCount';
 import RecurringDonationsTotalUsd from './RecurringDonationsTotalUsd';
 import DonationTokenStats from './DonationTokenStats';
 import VerificationDashboard from './VerificationDashboard';
+import { TABS, Tab } from './tabNames';
+import type { BackendVersion } from '../../../lib/requests';
 
 interface TabContentProps {
-	activeTab: string;
+	activeTab: Tab;
 }
+
+const DonationsTab = ({ version }: { version: BackendVersion }) => (
+	<>
+		<DonorsCount version={version} />
+		<hr />
+		<DonationsCount version={version} />
+		<hr />
+		<TotalDonations version={version} />
+		<hr />
+		<NewDonorsCount version={version} />
+		<hr />
+		<NewDonorsDonationTotalUsd version={version} />
+		<hr />
+		<DonationTokenStats version={version} />
+	</>
+);
 
 const TabContent: React.FC<TabContentProps> = ({ activeTab }) => {
 	return (
 		<>
-			{activeTab === 'Donations' && (
-				<>
-					<DonorsCount />
-					<hr />
-					<DonationsCount />
-					<hr />
-					<TotalDonations />
-					<hr />
-					<NewDonorsCount />
-					<hr />
-					<NewDonorsDonationTotalUsd />
-					<hr />
-					<DonationTokenStats />
-				</>
-			)}
-			{activeTab === 'Recurring Donations' && (
+			{activeTab === TABS.V5_DONATIONS && <DonationsTab version='v5' />}
+			{activeTab === TABS.V6_DONATIONS && <DonationsTab version='v6' />}
+			{activeTab === TABS.RECURRING_DONATIONS && (
 				<>
 					<RecurringDonationsCount />
 					<hr />
 					<RecurringDonationsTotalUsd />
 				</>
 			)}
-			{activeTab === 'Projects' && (
-				<>
-					<ProjectsCount />
-				</>
+			{activeTab === TABS.V5_PROJECTS && <ProjectsCount version='v5' />}
+			{activeTab === TABS.V6_PROJECTS && <ProjectsCount version='v6' />}
+			{activeTab === TABS.MULTISIG && <MultisigSessionsCount />}
+			{activeTab === TABS.V5_DONATION_BOX && (
+				<DonationBoxMetrics version='v5' />
 			)}
-			{activeTab === 'Multisig' && (
-				<>
-					<MultisigSessionsCount />
-				</>
+			{activeTab === TABS.V6_DONATION_BOX && (
+				<DonationBoxMetrics version='v6' />
 			)}
-			{activeTab === 'Optional Donation Box' && (
-				<>
-					<DonationBoxMetrics />
-				</>
-			)}
-			{activeTab === 'Verification' && (
-				<>
-					<VerificationDashboard />
-				</>
-			)}
+			{activeTab === TABS.VERIFICATION && <VerificationDashboard />}
 		</>
 	);
 };
